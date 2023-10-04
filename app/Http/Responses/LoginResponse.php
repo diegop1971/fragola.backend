d52f906cf62d79daff582efa870124177f614b4d
Diego Puccio <diego.puccio@gmail.com>
2023-10-04 10:26:16 -0300

    login json response antes devolvia el id (uuid) truncado (solo 3 caracteres), ahora lo devuelve completo

--- app/Http/Responses/LoginResponse.php
+++ app/Http/Responses/LoginResponse.php
@@ -3,19 +3,24 @@
 namespace App\Http\Responses;
 
 use Illuminate\Http\JsonResponse;
-use Laravel\Fortify\Contracts\LoginResponse as FortifyLoginResponse;
 use Illuminate\Support\Facades\Log;
+use Laravel\Fortify\Contracts\LoginResponse as FortifyLoginResponse;
 
 class LoginResponse implements FortifyLoginResponse
 {
     public function toResponse($request): JsonResponse
     {
-        Log::info('sorongueli');
         $user = $request->user();
         
         return response()->json([
             'success' => true,
-            'data' => $user,
+            'data' => [
+                'id' => $user->id,
+                'name' => $user->name,
+                'email' => $user->email,
+                //'avatar' => $user->avatar,
+                //'roles' => $user->roles,
+            ],
         ]);
     }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Ramsey\Uuid\Uuid;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -12,6 +13,9 @@ class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
 
+    protected $keyType = 'string';
+    public $incrementing = false;
+    
     /**
      * The attributes that are mass assignable.
      *
@@ -42,4 +46,13 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+    public static function boot()
+    {
+        parent::boot();
+
+        self::creating(function ($model) {
+            $model->id = Uuid::uuid4()->toString();
+        });
+    }
 }
